f5a5683b8e886e9fa96314d20fa894472b2528d9
Brian Bird <birdb@lanecc.edu>
2014-02-20 11:23:47 -0800

    Category menu and paging done
    Finished through page 218

--- SimpleSportsStore.WebUI/App_Start/RouteConfig.cs
+++ SimpleSportsStore.WebUI/App_Start/RouteConfig.cs
@@ -11,8 +11,12 @@ namespace SimpleSportsStore.WebUI
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            /* Note: routes are processed in the order they are entered below */
 
+            // ignore 
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            
+            // Example URL: http://webhost/
             routes.MapRoute(null,
                 "", 
                 new {
@@ -21,24 +25,54 @@ namespace SimpleSportsStore.WebUI
                 }
             );
 
+            /* Replaced by a new version in chapter 8
+            // Example URL: http://webhost/Page2 
+            routes.MapRoute(
+                name: null,
+                url: "Page{page}",
+                defaults: new { Controller = "Product", action = "List" }
+                );
+            */
+
+            // Example URL: http://webhost/Page2
+            // Parameter 2: string specifying the Url prameter name and, in curly braces,
+            //              the name of a variable that will contain the url parameter value
+            // Parameter 3: untyped object with default route values
+            // Parameter 4: untyped object with url parameter values
             routes.MapRoute(null,
                 "Page{page}", 
                 new { controller = "Product", action = "List", category = (string)null },
                 new { page = @"\d+" }
                 );
 
+            // Example URL: http://webhost/Soccer
             routes.MapRoute(null,
                 "{category}",
                 new { controller = "Product", action = "List", page = 1 }
             );
 
+            // Example URL: http://webhost/Soccer/Page2
             routes.MapRoute(null,
                 "{category}/Page{page}",
                 new { controller = "Product", action = "List" },
                 new { page = @"\d+" }
             );
 
+            // Example URL: http://anything/Else
             routes.MapRoute(null, "{controller}/{action}");
+
+            /* We are no longer using this default route
+            routes.MapRoute(name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Product", action = "List",
+                id = UrlParameter.Optional}
+                );
+            */
         }
     }
 }
+
+/* Note: In the Default route, named arguments are being used in the call
+ * to MapRoute. See Named and Optional Arguments (C# Programming Guide)
+ * http://msdn.microsoft.com/en-us/library/dd264739.aspx
+ */
--- SimpleSportsStore.WebUI/Controllers/ProductController.cs
+++ SimpleSportsStore.WebUI/Controllers/ProductController.cs
@@ -34,24 +34,18 @@ namespace SimpleSportsStore.WebUI.Controllers
                 .Take(PageSize)
                 );
              */
-            ProductsListViewModel model = new ProductsListViewModel
-            {
+            ProductsListViewModel model = new ProductsListViewModel {
                 // Get a list of products, put it in the model
                 Products = repository.Products
-                .Where(p => category == null || p.Category == category) // filter by category
-                .OrderBy(p => p.ProductID)
-                .Skip((page - 1) * PageSize)
-                .Take(PageSize),
-
-                // Put the paging info in the model too
-                PagingInfo = new PagingInfo 
-                {
+                    .Where(p => category == null || p.Category == category) // filter by category
+                    .OrderBy(p => p.ProductID)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo {
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
-                    TotalItems = repository.Products.Count()
-                },
-
-                // Set the current category which the user selected
+                    TotalItems =  category == null ? repository.Products.Count() :
+                                                repository.Products.Where(e => e.Category == category).Count() },
                 CurrentCategory = category
             };
 
